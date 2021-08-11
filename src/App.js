f5a513f9a035d8f2c7868ea38ba99a8e3840e483
Muhsin <paravathmuhsin@gmail.com>
2021-08-11 08:25:28 +0530

    add lazy loading

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import { CssBaseline } from "@material-ui/core";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import "./App.css";
-import Home from "./containers/Home/Home";
-import React from "react";
+import React, { Suspense } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
-import JobDetails from "./containers/JobDetails/JobDetails";
 import AppContext from "./core/AppContext";
-import Login from "./containers/Login/Login";
 import ProtectedRouter from "./core/ProtectedRouter";
 
 function App() {
+  const Home = React.lazy(() => import("./containers/Home/Home"));
+  const Login = React.lazy(() => import("./containers/Login/Login"));
+  const JobDetails = React.lazy(() =>
+    import("./containers/JobDetails/JobDetails")
+  );
+
   return (
     <BrowserRouter>
       <AppContext>
         <div className="App-root">
           <CssBaseline />
           <Header />
-          <Switch>
-            <ProtectedRouter  exact path="/" component={Home}  />
-            <Route path="/login" component={Login} />
-            <ProtectedRouter  path="/job/:id" component={JobDetails}  />
-          </Switch>
+          <Suspense fallback={<h2>Loading....</h2>}>
+            <Switch>
+              <ProtectedRouter exact path="/" component={Home} />
+              <Route path="/login" component={Login} />
+              <ProtectedRouter path="/job/:id" component={JobDetails} />
+              <Route render={() => <Redirect to="/" />} />
+            </Switch>
+          </Suspense>
         </div>
       </AppContext>
     </BrowserRouter>
